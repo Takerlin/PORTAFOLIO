f6c262d1e5ebdfbeaf99d1b06cf52e8282a746ac
Takerlin Gereidi <takerlincalderon@outlook.com>
2022-12-01 19:18:43 -0800

    cambio

---
+++ PORTAFOLIO/Controllers/TipoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PORTAFOLIO.Models;
+
+namespace PORTAFOLIO.Controllers
+{
+    public class TipoesController : Controller
+    {
+        private portafolioEntities db = new portafolioEntities();
+
+        // GET: Tipoes
+        public ActionResult Index()
+        {
+            return View(db.Tipo.ToList());
+        }
+
+        // GET: Tipoes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipo tipo = db.Tipo.Find(id);
+            if (tipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipo);
+        }
+
+        // GET: Tipoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tipoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion")] Tipo tipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tipo.Add(tipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tipo);
+        }
+
+        // GET: Tipoes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipo tipo = db.Tipo.Find(id);
+            if (tipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipo);
+        }
+
+        // POST: Tipoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion")] Tipo tipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tipo);
+        }
+
+        // GET: Tipoes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipo tipo = db.Tipo.Find(id);
+            if (tipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipo);
+        }
+
+        // POST: Tipoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Tipo tipo = db.Tipo.Find(id);
+            db.Tipo.Remove(tipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
